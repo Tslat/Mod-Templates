a23d2223c10be8e200724c79ba69b7cd3b6f9ada
Tslat <7098525+Tslat@users.noreply.github.com>
2026-01-28 12:29:20 +1000

    Fix outdated event bus handling for Forge

--- forge/src/main/java/com/github/myname/mymod/ModMain.java
+++ forge/src/main/java/com/github/myname/mymod/ModMain.java
@@ -2,7 +2,7 @@ package com.github.myname.mymod;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,9 +12,9 @@ public class ModMain {
     public static final DeferredRegister<Item> ITEM_REGISTRY = DeferredRegister.create(Registries.ITEM, ModConstants.MODID);
 
     public ModMain(FMLJavaModLoadingContext context) {
-        final IEventBus modBus = context.getModEventBus();
+        final BusGroup busGroup = context.getModBusGroup();
 
-        ITEM_REGISTRY.register(modBus);
+        ITEM_REGISTRY.register(busGroup);
 
         ModCommon.init();
     }
