d19e0f1caa7af433e07db16d207e898ff8e92718
Tslat <7098525+Tslat@users.noreply.github.com>
2026-05-28 06:49:42 +1000

    Remove unnecessary AT plugin

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.curseforgegradle)
     alias(libs.plugins.forgegradle)
     alias(libs.plugins.forge.jarjar)
-    alias(libs.plugins.forge.at)
 }
 
 val modId: String by project
