6c14eee26e387f8ccfe932cf201abd92a0cb2a14
Tslat <7098525+Tslat@users.noreply.github.com>
2026-06-20 20:35:48 +1000

    Found fix for parallel mode

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -24,10 +24,10 @@ minecraft {
             workingDir.convention(layout.projectDirectory.dir("runs/${name}"))
             systemProperty("forge.logging.console.level", "debug")
 
-            mods.create(modId) {
+            /*mods.create(modId) {
                 source(sourceSets.main.get())
                 source(project(":common").sourceSets.main.get())
-            }
+            }*/
         }
 
         register("client") {
