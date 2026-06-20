2fdc5590466541e59c55b609f7ecb26fca89c0f0
Tslat <7098525+Tslat@users.noreply.github.com>
2026-06-20 19:24:43 +1000

    Remove renamer

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     alias(libs.plugins.loom) apply false
     alias(libs.plugins.forgegradle) apply false
     alias(libs.plugins.forge.jarjar) apply false
-    alias(libs.plugins.forge.renamer) apply false
 
     alias(libs.plugins.minotaur) apply false
     alias(libs.plugins.curseforgegradle) apply false
